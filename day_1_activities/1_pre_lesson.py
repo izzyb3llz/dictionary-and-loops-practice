eb586746e72ec5899eb8d430815253f73f8a2a19
izzyb3llz <isacontreras519@gmail.com>
2025-12-15 20:08:17 +0000

    Lesson 1

--- day_1_activities/1_pre_lesson.py
+++ day_1_activities/1_pre_lesson.py
@@ -1,1 +1,34 @@
 # this is what we will use for the video intro to dictionaries
+# dictionary = a collection of {key:value} pairs
+#              ordered and changedable, no duplicates.
+capitals = {"USA": "Washiongton D.C.",
+            "India": "New Dehli",
+            "China": "Bejing",
+            "Russia": "Moscow"}
+
+print(dir(capitals))
+print(help(captials))
+print(capitals.get("Japan"))
+if captial.get ("Japan")
+    print("That captial exists.")
+else:
+    print("That capital does not exists.")
+
+captials.update(("Germany": "Berlin"))
+captials.update(("USA": "Detroit"))
+Captials.pop("China")
+captials.popitem()
+captials.clear()
+
+keys = captials.keys()
+print(captials)
+for key in captials.key
+    print(key)
+
+values = capitals.values()
+for values in caplitals.values
+    print(values)
+
+items = captials.items()
+for key, values in capitals.items():
+    print(f"{key}: {value}")
